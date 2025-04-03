72abaa4cc0ade469277f629208ff6c36d77d2412
Jokoson <exampleexampleexample12135example@example.com>
2025-04-03 10:38:15 +0200

    feat: display route parameter id in test.component and log for debugging

--- src/app/router/test/test.component.ts
+++ src/app/router/test/test.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiComponent} from '../../api/api.component';
 import {HttpClient} from '@angular/common/http';
 import {Meta, Title} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-test',
@@ -11,9 +12,18 @@ import {Meta, Title} from '@angular/platform-browser';
   templateUrl: './test.component.html',
   styleUrl: './test.component.scss'
 })
-export class TestComponent {
-  constructor(private metaService: Meta, private titleService: Title) {
+export class TestComponent implements OnInit {
+  id: string | null = null;
+
+  constructor(private metaService: Meta, private titleService: Title, private route: ActivatedRoute) {
     this.titleService.setTitle('Mein Seitentitel');
     this.metaService.addTag({ name: 'description', content: 'Meine Seitenbeschreibung' });
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.id = params.get('id');
+      console.log(this.id);
+    });
   }
 }
