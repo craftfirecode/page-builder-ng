c21acc99a04487cc9167d45072065c27ee094aac
Jokoson <exampleexampleexample12135example@example.com>
2025-04-03 08:14:44 +0200

    add day name display and ngSwitch functionality to api component

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {JsonPipe} from "@angular/common";
+import {Component, OnInit} from '@angular/core';
+import {JsonPipe, NgSwitch, NgSwitchCase, NgSwitchDefault} from "@angular/common";
 import {HttpClient} from '@angular/common/http';
 import {MatButton} from '@angular/material/button';
 
@@ -7,13 +7,17 @@ import {MatButton} from '@angular/material/button';
   selector: 'app-api',
   imports: [
     JsonPipe,
-    MatButton
+    MatButton,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault
   ],
   templateUrl: './api.component.html',
   styleUrl: './api.component.scss'
 })
-export class ApiComponent {
+export class ApiComponent implements OnInit {
   data = []
+  dayName: string | undefined;
 
   constructor(private http: HttpClient) {
     http.get<any>('https://jsonplaceholder.typicode.com/posts/1').subscribe(res => {
@@ -21,4 +25,34 @@ export class ApiComponent {
       this.data = res;
     });
   }
+
+  ngOnInit(): void {
+    const day: number = new Date().getDay();
+    switch (day) {
+      case 0:
+        this.dayName = 'Sunday';
+        break;
+      case 1:
+        this.dayName = 'Monday';
+        break;
+      case 2:
+        this.dayName = 'Tuesday';
+        break;
+      case 3:
+        this.dayName = 'Wednesday';
+        break;
+      case 4:
+        this.dayName = 'Thursday';
+        break;
+      case 5:
+        this.dayName = 'Friday';
+        break;
+      case 6:
+        this.dayName = 'Saturday';
+        break;
+      default:
+        this.dayName = 'Invalid day';
+    }
+  }
+
 }
