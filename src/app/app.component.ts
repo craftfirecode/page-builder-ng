f3763c032bbacb68de6a4d2023222acf88f64899
Jokoson <exampleexampleexample12135example@example.com>
2025-04-03 09:18:01 +0200

    feat: implement responsive sidenav in app.component for improved navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {ApiComponent} from './api/api.component';
+import {MatDrawerContainer, MatSidenavModule} from '@angular/material/sidenav';
+import {MatButton, MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatSidenavModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'page-builder-ng';
+  showFiller = false;
 }
