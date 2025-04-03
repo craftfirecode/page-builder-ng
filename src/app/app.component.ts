5c8577dfa3d8f3d6bc1847dd9f91d883d659e225
Jokoson <exampleexampleexample12135example@example.com>
2025-04-03 08:42:43 +0200

    feat: enhance navigation in app.component with router links and active states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {ApiComponent} from './api/api.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
