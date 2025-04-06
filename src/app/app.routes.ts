eced3c630550660eee6931275f5bcf2934660ac0
Jokoson <exampleexampleexample12135example@example.com>
2025-04-06 19:39:20 +0200

    feat: update routing to include portfolio and blog components with dynamic URL handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
 import { TestComponent } from './router/test/test.component';
 import { IndexComponent } from './router/index/index.component';
+import {PortfolioListComponent} from './router/portfolio-list/portfolio-list.component';
+import {PortfolioComponent} from './router/portfolio/portfolio.component';
+import {BlogListComponent} from './router/blog-list/blog-list.component';
+import {BlogComponent} from './router/blog/blog.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
-  { path: ':pageName', component: TestComponent },
+  { path: 'portfolio', component: PortfolioListComponent },
+  { path: 'portfolio/:url', component: PortfolioComponent },
+  { path: 'blog', component: BlogListComponent },
+  { path: 'blog/:url', component: BlogComponent },
+  { path: ':url', component: TestComponent }
 ];
