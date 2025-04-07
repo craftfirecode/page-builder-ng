cb3698ee22fe3981e77dba0c6783e3f20d00d650
Jokoson <exampleexampleexample12135example@example.com>
2025-04-07 09:23:59 +0200

    feat: add Badge component and enhance blog/portfolio list layouts

--- src/app/router/blog-list/blog-list.component.ts
+++ src/app/router/blog-list/blog-list.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {BadgeComponent} from "../../components/ui/badge/badge.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {Meta, Title} from '@angular/platform-browser';
+import {ApiService} from '../../api.service';
 
 @Component({
   selector: 'app-blog-list',
   imports: [
-    RouterLink
+    RouterLink,
+    BadgeComponent,
+    NgForOf,
+    NgIf
   ],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.scss'
 })
-export class BlogListComponent {
+export class BlogListComponent implements OnInit {
+  strapiData: any;
 
+  constructor(private metaService: Meta, private titleService: Title, private route: ActivatedRoute, private apiService: ApiService) {
+    this.titleService.setTitle('Portfolio');
+    this.metaService.addTag({name: 'description', content: 'Meine Seitenbeschreibung'});
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.apiService.getBlogListData().subscribe(res => {
+        this.strapiData = res.data;
+      });
+    });
+  }
 }
--- src/app/router/portfolio-list/portfolio-list.component.ts
+++ src/app/router/portfolio-list/portfolio-list.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {NgForOf, NgIf} from '@angular/common';
+import {Meta, Title} from '@angular/platform-browser';
+import {ApiService} from '../../api.service';
+import {BadgeComponent} from '../../components/ui/badge/badge.component';
 
 @Component({
   selector: 'app-portfolio-list',
   imports: [
-    RouterLink
+    RouterLink,
+    NgForOf,
+    NgIf,
+    BadgeComponent
   ],
   templateUrl: './portfolio-list.component.html',
   styleUrl: './portfolio-list.component.scss'
 })
-export class PortfolioListComponent {
+export class PortfolioListComponent implements OnInit {
+  strapiData: any;
 
+  constructor(private metaService: Meta, private titleService: Title, private route: ActivatedRoute, private apiService: ApiService) {
+    this.titleService.setTitle('Portfolio');
+    this.metaService.addTag({name: 'description', content: 'Meine Seitenbeschreibung'});
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.apiService.getPostListData().subscribe(res => {
+        this.strapiData = res.data;
+      });
+    });
+  }
 }
