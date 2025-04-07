3738608c8983e3fbc3bffb9ff687969f54babba8
Jokoson <exampleexampleexample12135example@example.com>
2025-04-07 12:49:14 +0200

    feat: update default server port to 4099 and clean up import formatting

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
+import {dirname, resolve} from 'node:path';
+import {fileURLToPath} from 'node:url';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -54,7 +54,7 @@ app.use('/**', (req, res, next) => {
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4099;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
