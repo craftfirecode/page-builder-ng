924d4981cd500c38164538d55673f0641ad058ab
Jokoson <exampleexampleexample12135example@example.com>
2025-06-08 12:30:41 +0200

    add PrimeNG support with animations and theme configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,18 @@ import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {icons, LucideAngularModule} from 'lucide-angular';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    }),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withFetch()),
